aa78160977ff74092607815f06a68a12bcda6463
atefm <atef.mannai@esprit.tn>
2024-04-16 17:28:26 +0100

    login succes

--- src/Form/ModifierType.php
+++ src/Form/ModifierType.php
@@ -4,50 +4,35 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
-
-class UserType extends AbstractType
+class ModifierType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email')
+            ->add('password')
             ->add('nom')
             ->add('prenom')
             ->add('adresse')
             ->add('telephone')
-            ->add('role', ChoiceType::class, [
-                'choices' => [
-                    'Contributeur' => 'contributeur',
-                    'Collecteur' => 'collecteur',
-                    'Société' => 'societe',
-                ],
-                'placeholder' => 'Sélectionner un rôle',
-                'required' => true,
-            ])
-            ->add('password')
-            ->add('verifcode')
-            ->add('cin')
-            ->add('matFiscal')
             ->add('rib')
-            ->add('email')
-            ->add('userName')
             ->add('imageFile', VichImageType::class, [
                 'label' => 'Télécharger une image', // Étiquette personnalisée
                 'allow_delete' => false, // Désactiver l'option de suppression
                 'download_uri' => false, // Désactiver l'option de téléchargement
                 'image_uri' => false, // Désactiver l'affichage de l'image
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-
+            'data_class' => User::class,
         ]);
     }
 }
