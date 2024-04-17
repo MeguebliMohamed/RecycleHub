1c6bce567bae3a5c2fe9d8bbfb8f29b8d8f2e2a6
atefm <atef.mannai@esprit.tn>
2024-04-17 16:32:15 +0100

    recherche multicritaire (captcha , mail non fonctionnel )

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Captcha\Bundle\CaptchaBundle\CaptchaBundle::class => ['all' => true],
 ];
