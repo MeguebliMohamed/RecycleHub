9eb95428a8afd13f5fb21fad04235081c171946a
mohamed anouar <mohamedanoiar.meguebli@esprit.tn>
2024-05-06 22:24:04 +0100

    verssion 5/6/2024

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\User1Type;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/profile')]
+class ProfileController extends AbstractController
+{
+    #[Route('', name: 'app_profile_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(User1Type::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashPassword = $passwordHasher->hashPassword($user,$plainPassword);
+            $user->setPassword($hashPassword);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+
+}
