e76560da4703120d179bbc751bea790efc8d5d4d
mohamed anouar <mohamedanoiar.meguebli@esprit.tn>
2024-05-06 22:31:20 +0100

    asas

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{/*
+    /**
+     * @Route("/",name="app_home")
+     */
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('FrontTemplate.html.twig');
+        /*return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    */
+        }
+}
